53e59706b2c46eca65ba5308498c5f7822ac2c8d
sydz92 <sydz92@gmail.com>
2017-05-13 15:03:41 -0300

    test completo pb2hal sin probar

--- src/hal/pb2/pb2haltest.cpp
+++ src/hal/pb2/pb2haltest.cpp
@@ -18,11 +18,29 @@ int main(int argc, char** argv){
 	/************** MOVIMIENTO *****************/
 
 
+	//Imagen
+	cout << "Probando imagen..." << endl;
+	cv::Mat* res = hal.getFrame(Camera::Front);
+	cv::namedWindow( "Display window", cv::WINDOW_AUTOSIZE );// Create a window for display.
+    cv::imshow( "Display window", *res );                   // Show our image inside it.
+    cv::waitKey(0);
+
+    //Altura
+	cout << "Altura: " << hal.getAltitude() << endl;
+
+	//Orientacion
+	Point o = hal.getOrientation();
+	cout << "Orientación: [" << o.x << ", " << o.y << ", " << o.z << "]" << endl;
+
+	//Posicion
+	Point p = hal.getOrientation();
+	cout << "Posición: [" << p.x << ", " << p.y << ", " << p.z << "]" << endl;
+
 	//despegar
 	cout << "Despegar..." << endl;
 	hal.takeoff();
 
-	cout << "Esperar 8s..." << endl;
+	cout << "Esperar 4s..." << endl;
 	sleep(4);
 
 	//aterrizar
