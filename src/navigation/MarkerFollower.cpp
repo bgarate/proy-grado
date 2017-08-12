4e39705774e355e6095a471df308e73041f54ffe
Bruno Garate <bruno.garate@gmail.com>
2017-08-12 17:48:25 -0300

    OpenCV y OpenGL

---
+++ src/navigation/MarkerFollower.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bruno on 12/08/17.
+//
+
+#include "MarkerFollower.h"
---
+++ src/navigation/MarkerFollower.h
@@ -0,0 +1,16 @@
+//
+// Created by bruno on 12/08/17.
+//
+
+#ifndef PROY_GRADO_MARKERFOLLOWER_H
+#define PROY_GRADO_MARKERFOLLOWER_H
+
+
+
+class MarkerFollower {
+
+};
+
+
+
+#endif //PROY_GRADO_MARKERFOLLOWER_H
